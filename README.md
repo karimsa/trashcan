2de87e447505922733f93ecc065c2a17d39a99c7
Karim Alibhai <k4rim.sa@gmail.com>
2015-04-24 08:51:27 -0400

    initial

---
+++ README.md
@@ -0,0 +1,88 @@
+# Trashcan
+
+Safe error handling in production mode.
+
+[![NPM](https://nodei.co/npm/trashcan.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/trashcan/)
+
+## Usage
+
+Install via npm (`npm install --save trashcan`) and then take advantage of the API to .
+
+## Table of Contents
+
+ - [Error Handling](#error-handling)
+ - [Asynchronous Callback Errors](#asynchronous-callbacks)
+ - [Synchronous Errors](#synchronous-errors)
+ - [Custom Errors](#custom-errors)
+
+### Error Handling
+
+```javascript
+var tc = require('trashcan')
+
+// handle the errors manually
+tc.on('error', function (err) {
+  // do stuff with the error
+})
+
+// or auto-email the admin
+tc.on('error', tc.notify('me@email.com'))
+
+// or to a server log
+tc.on('error', tc.log('./server.log'))
+```
+
+### Asynchronous Callback Errors
+
+```javascript
+var tc = require('trashcan')
+  , fs = require('fs')
+
+fs.readFile('./my-file.txt', 'utf8', tc(function (data) {
+  // handle file data
+}))
+```
+
+### Synchronous Errors
+
+```javascript
+var tc = require('trashcan')
+  , fs = require('fs')
+
+tc(function () {
+  var data = fs.readFileSync('./my-file.txt', 'utf8')
+
+  // handle file data
+}).exec()
+```
+
+### Custom Errors
+
+```javascript
+var tc = require('trashcan')
+
+// throw the error manually
+tc.throw(trash)
+```
+
+## License
+
+GPLv3.
+
+```
+trashcan: safe error handling in production mode.
+Copyright (C) 2015 Online Health Database
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+```
---
+++ index.js
@@ -0,0 +1,161 @@
+/**
+ * @file index.js
+ * @project trashcan
+ * @license GPLv3.
+ * @copyright 2015 Online Health Database.
+ */
+
+"use strict";
+
+var rc = require('rc')
+  , events = new (require('events').EventEmitter)
+  , nodemailer = require('nodemailer')
+  , timestamp = function (msg) {
+      return '[' + (new Date) + '] ' + msg
+    }
+  , tc = function (fn, context) {
+      context = context || tc
+
+      var handle = function (error) {
+        if (error) {
+          tc.throw(error)
+        } else {
+          var args = Array.prototype.slice.call(arguments, 1)
+          return fn.apply(context, args)
+        }
+      }
+
+      /**
+       * @memberof Handler
+       */
+      handle.exec = function () {
+        try {
+          fn.apply(context, arguments)
+        } catch (error) {
+          tc.throw(error)
+        }
+      }
+
+      return handle
+    }
+  , prototype = {
+
+      /**
+       * Attach an event listener.
+       * @memberof trashcan
+       * @method on
+       * @params {String} event - the event to attach to
+       * @params {Function} callback - an event handler
+       */
+      on: function () {
+        return events.on.apply(events, arguments)
+      }
+
+      /**
+       * Detach an event listener.
+       * @memberof trashcan
+       * @method off
+       * @params {String} event - the event to detach from
+       * @params {Function} callback - an event handler
+       */
+    , off: function () {
+        return events.off.apply(events, arguments)
+      }
+
+      /**
+       * Emit an event.
+       * @memberof trashcan
+       * @method emit
+       * @params {String} event - the event to emit
+       * @params {Variant} data - some data to pass
+       */
+    , emit: function () {
+        return events.emit.apply(events, arguments)
+      }
+
+      /**
+       * Expand an error into its full form (with extra details).
+       * @memberof trashcan
+       * @method full
+       * @params {String} error - an error message
+       * @returns {String} message - an expanded error message
+       */
+    , full: function (error) {
+        return JSON.stringify({
+            error: error
+          , freemem: 0
+        }, null, 2)
+      }
+
+      /**
+       * Throw an error safely.
+       * @memberof trashcan
+       * @method throw
+       * @params {Error|String} error - the error to throw
+       */
+    , throw: function (error) {
+        return this.emit('error', error)
+      }
+
+      /**
+       * Create a callback for logging errors to a file.
+       * @memberof trashcan
+       * @method log
+       * @params {String} filename - the file to log to
+       * @returns {Function} callback - the callback to aattach to the error event
+       */
+    , log: function (filename) {
+        var self
+        fs.writeFileSync(filename, timestamp('Error log started.') + '\n')
+
+        /** simply append all error info to the file */
+        return (self = function (error) {
+          fs.appendFile(filename, timestamp(tc.full(error)) + '\n')
+          return self
+        })
+      }
+
+      /**
+       * Create a callback for sending errors as emails.
+       * @memberof trashcan
+       * @method notify
+       * @params {Array|String} email(s) - the email address(es) to notify
+       * @returns {Function} callback - the callback to attach to the error event
+       */
+    , notify: function (emails, config) {
+        var self, transport
+
+        // prefer arrays
+        if (typeof emails === 'string') emails = [emails]
+
+        // try loading mail configuration
+        if (!config) config = rc('mail', {})
+
+        // create a nodemailer transporter
+        transport = nodemailer.createTransport(config)
+
+        /** use nodemailer to transport a full notification */
+        return (self = function (error) {
+          transport.sendMail({
+              to: emails
+            , from: config.auth.user
+            , subject: String(error)
+            , text: tc.full(error)
+            , html: '<code><pre>' + tc.full(error) + '</pre></code>'
+          })
+  
+          return self
+        })
+      }
+    }
+  , i
+
+// expand prototype onto function
+for (i in prototype) {
+  if (prototype.hasOwnProperty(i)) {
+    tc[i] = prototype[i]
+  }
+}
+
+/** @module trashcan */
+module.exports = tc
